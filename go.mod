9d4783c168143714217dd307cc847108289f346d
Conrad Kurth <conradkurth@gmail.com>
2019-05-02 13:54:50 -0700

    slimmed down elasticsearch provider

---
+++ go.mod
@@ -0,0 +1,60 @@
+module github.com/ConradKurth/terraform-provider-elasticsearch
+
+go 1.12
+
+require (
+	github.com/agext/levenshtein v1.2.1 // indirect
+	github.com/apparentlymart/go-cidr v0.0.0-20170616213631-2bd8b58cf427 // indirect
+	github.com/apparentlymart/go-textseg v0.0.0-20170531203952-b836f5c4d331 // indirect
+	github.com/armon/go-radix v0.0.0-20170727155443-1fca145dffbc // indirect
+	github.com/aws/aws-sdk-go v1.8.36
+	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d // indirect
+	github.com/bgentry/speakeasy v0.1.0 // indirect
+	github.com/blang/semver v0.0.0-20180305174007-c5e971dbed78 // indirect
+	github.com/davecgh/go-spew v0.0.0-20151105211317-5215b55f46b2 // indirect
+	github.com/deoxxa/aws_signing_client v0.0.0-20161109131055-c20ee106809e
+	github.com/fatih/color v1.6.0 // indirect
+	github.com/fortytw2/leaktest v1.3.0 // indirect
+	github.com/go-ini/ini v0.0.0-20170519023713-afbc45e87f3b // indirect
+	github.com/go-test/deep v1.0.1 // indirect
+	github.com/golang/protobuf v0.0.0-20170914231319-11b8df160996 // indirect
+	github.com/google/go-cmp v0.3.0 // indirect
+	github.com/hashicorp/errwrap v0.0.0-20141028054710-7554cd9344ce // indirect
+	github.com/hashicorp/go-cleanhttp v0.0.0-20170211013415-3573b8b52aa7 // indirect
+	github.com/hashicorp/go-getter v0.0.0-20170914154444-56c651a79a6e // indirect
+	github.com/hashicorp/go-hclog v0.0.0-20170903163258-8105cc0a3736 // indirect
+	github.com/hashicorp/go-multierror v0.0.0-20170622060955-83588e72410a // indirect
+	github.com/hashicorp/go-plugin v0.0.0-20170828024549-3e6d191694b5 // indirect
+	github.com/hashicorp/go-uuid v0.0.0-20160717022140-64130c7a86d7 // indirect
+	github.com/hashicorp/go-version v0.0.0-20170914154128-fc61389e27c7 // indirect
+	github.com/hashicorp/hcl v0.0.0-20170914154624-68e816d1c783 // indirect
+	github.com/hashicorp/hcl2 v0.0.0-20180406023453-9db880accff1 // indirect
+	github.com/hashicorp/hil v0.0.0-20170627220502-fa9f258a9250 // indirect
+	github.com/hashicorp/logutils v0.0.0-20150609070431-0dc08b1671f3 // indirect
+	github.com/hashicorp/terraform v0.11.6
+	github.com/hashicorp/yamux v0.0.0-20160720233140-d1caa6c97c9f // indirect
+	github.com/jmespath/go-jmespath v0.0.0-20160803190731-bd40a432e4c7 // indirect
+	github.com/kylelemons/godebug v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.0 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/mitchellh/cli v0.0.0-20180414170447-c48282d14eba // indirect
+	github.com/mitchellh/copystructure v0.0.0-20170525013902-d23ffcb85de3 // indirect
+	github.com/mitchellh/go-homedir v0.0.0-20161203194507-b8bc1bf76747 // indirect
+	github.com/mitchellh/go-testing-interface v0.0.0-20170901155038-7bf6f6eaf1be // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/mitchellh/hashstructure v0.0.0-20160609170929-b098c52ef6be // indirect
+	github.com/mitchellh/mapstructure v0.0.0-20170523030023-d0303fe80992 // indirect
+	github.com/mitchellh/reflectwalk v0.0.0-20170726202117-63d60e9d0dbc // indirect
+	github.com/olivere/elastic v6.2.17+incompatible
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/posener/complete v0.0.0-20180413091813-e037c22b2fcf // indirect
+	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	github.com/ulikunitz/xz v0.5.4 // indirect
+	github.com/zclconf/go-cty v0.0.0-20180328152515-d006e4534bc4 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
+	google.golang.org/genproto v0.0.0-20170918111702-1e559d0a00ee // indirect
+	google.golang.org/grpc v0.0.0-20170915004038-bb78878767b9 // indirect
+	gopkg.in/olivere/elastic.v5 v5.0.80
+	gopkg.in/olivere/elastic.v6 v6.2.17
+)
--- provider.go
+++ provider.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"context"
-	"crypto/tls"
-	"crypto/x509"
-	"log"
-	"net/http"
 	"net/url"
 	"regexp"
 
-	awscredentials "github.com/aws/aws-sdk-go/aws/credentials"
-	awssigv4 "github.com/aws/aws-sdk-go/aws/signer/v4"
-	"github.com/deoxxa/aws_signing_client"
-	"github.com/hashicorp/terraform/helper/pathorcontents"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
-	elastic5 "gopkg.in/olivere/elastic.v5"
-	elastic6 "gopkg.in/olivere/elastic.v6"
+	"github.com/olivere/elastic"
 )
 
 var awsUrlRegexp = regexp.MustCompile(`([a-z0-9-]+).es.amazonaws.com$`)
@@ -68,110 +58,28 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
-			"elasticsearch_index_template":      resourceElasticsearchIndexTemplate(),
-			"elasticsearch_snapshot_repository": resourceElasticsearchSnapshotRepository(),
-			"elasticsearch_kibana_object":       resourceElasticsearchKibanaObject(),
+			"elasticsearch_index_template": resourceElasticsearchIndexTemplate(),
+			// "elasticsearch_snapshot_repository": resourceElasticsearchSnapshotRepository(),
+			// "elasticsearch_kibana_object":       resourceElasticsearchKibanaObject(),
 		},
 
 		ConfigureFunc: providerConfigure,
 	}
+
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+
 	rawUrl := d.Get("url").(string)
-	insecure := d.Get("insecure").(bool)
-	cacertFile := d.Get("cacert_file").(string)
 	parsedUrl, err := url.Parse(rawUrl)
 	if err != nil {
 		return nil, err
 	}
-	opts := []elastic6.ClientOptionFunc{
-		elastic6.SetURL(rawUrl),
-		elastic6.SetScheme(parsedUrl.Scheme),
+
+	opts := []elastic.ClientOptionFunc{
+		elastic.SetURL(rawUrl),
+		elastic.SetScheme(parsedUrl.Scheme),
 	}
 
-	if m := awsUrlRegexp.FindStringSubmatch(parsedUrl.Hostname()); m != nil {
-		log.Printf("[INFO] Using AWS: %+v", m[1])
-		opts = append(opts, elastic6.SetHttpClient(awsHttpClient(m[1], d)), elastic6.SetSniff(false))
-	} else if insecure || cacertFile != "" {
-		opts = append(opts, elastic6.SetHttpClient(tlsHttpClient(d)), elastic6.SetSniff(false))
-	}
-
-	var relevantClient interface{}
-	client, err := elastic6.NewClient(opts...)
-	if err != nil {
-		return nil, err
-	}
-	relevantClient = client
-
-	// Use the v6 client to ping the cluster to determine the version
-	info, _, err := client.Ping(rawUrl).Do(context.TODO())
-	if err != nil {
-		return nil, err
-	}
-	if info.Version.Number < "6.0.0" {
-		log.Printf("[INFO] Using ES 5")
-		opts := []elastic5.ClientOptionFunc{
-			elastic5.SetURL(rawUrl),
-			elastic5.SetScheme(parsedUrl.Scheme),
-		}
-
-		if m := awsUrlRegexp.FindStringSubmatch(parsedUrl.Hostname()); m != nil {
-			opts = append(opts, elastic5.SetHttpClient(awsHttpClient(m[1], d)), elastic5.SetSniff(false))
-		} else if insecure || cacertFile != "" {
-			opts = append(opts, elastic5.SetHttpClient(tlsHttpClient(d)), elastic5.SetSniff(false))
-		}
-		relevantClient, err = elastic5.NewClient(opts...)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return relevantClient, nil
-}
-
-func awsHttpClient(region string, d *schema.ResourceData) *http.Client {
-	creds := awscredentials.NewChainCredentials([]awscredentials.Provider{
-		&awscredentials.StaticProvider{
-			Value: awscredentials.Value{
-				AccessKeyID:     d.Get("aws_access_key").(string),
-				SecretAccessKey: d.Get("aws_secret_key").(string),
-				SessionToken:    d.Get("aws_token").(string),
-			},
-		},
-		&awscredentials.EnvProvider{},
-		&awscredentials.SharedCredentialsProvider{},
-	})
-	signer := awssigv4.NewSigner(creds)
-	client, _ := aws_signing_client.New(signer, nil, "es", region)
-
-	return client
-}
-
-func tlsHttpClient(d *schema.ResourceData) *http.Client {
-	insecure := d.Get("insecure").(bool)
-	cacertFile := d.Get("cacert_file").(string)
-
-	// Configure TLS/SSL
-	tlsConfig := &tls.Config{}
-
-	// If a cacertFile has been specified, use that for cert validation
-	if cacertFile != "" {
-		caCert, _, _ := pathorcontents.Read(cacertFile)
-
-		caCertPool := x509.NewCertPool()
-		caCertPool.AppendCertsFromPEM([]byte(caCert))
-		tlsConfig.RootCAs = caCertPool
-	}
-
-	// If configured as insecure, turn off SSL verification
-	if insecure {
-		tlsConfig.InsecureSkipVerify = true
-	}
-
-	transport := &http.Transport{TLSClientConfig: tlsConfig}
-
-	client := &http.Client{Transport: transport}
-
-	return client
+	return elastic.NewSimpleClient(opts...)
 }
